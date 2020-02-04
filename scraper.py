44f26de210144f0fab5c054fd1d587d5b2e35368
MrFlopflop <henkhetvogeltje@gmail.com>
2020-02-04 01:28:38 +0100

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,73 @@
+import smtplib
+from time import sleep
+from selenium import webdriver
+from secrets import email, password
+
+cheapest = [9999, None]
+
+driver = webdriver.Chrome()
+
+class Flight:
+    def __init__(self, dict, cheapest):
+        self.toDates = dict['toDates']
+        self.fromDates = dict['fromDates']
+        self.cheapest = cheapest
+        self.URL1 = dict['url1']
+        self.middleBit = dict['middleBit']
+        self.URL2 = dict['url2']
+        self.to = dict['to']
+        self.type = dict['type']
+        self.price = dict['price']
+
+    def check_price(self, toDate, fromDate):
+        if self.type == 'enkele':
+            driver.get(self.URL1 + toDate + self.URL2)
+            sleep(0.5)
+        elif self.type == 'retour':
+            driver.get(self.URL1 + toDate + self.middleBit + fromDate + self.URL2)
+            sleep(0.5)
+        driver.implicitly_wait(10)
+        price = driver.find_element_by_css_selector('.gws-flights-results__cheapest-price').text
+        converted_price = int(price[2:])
+        if self.cheapest[0] <= converted_price:
+            return self.cheapest
+        else:
+            return [converted_price, [toDate, fromDate]]
+
+    def send_mail(self):
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(email, password)
+
+        subject = 'Goedkope vlucht'
+        if self.type == 'enkele':
+            body = "Er is een " + self.type + " vlucht van " + str(self.cheapest[0]) + " euro naar " + self.to + " \n"\
+               + self.URL1 + self.cheapest[1][0] + self.URL2
+        elif self.type == 'retour':
+            body = "Er is een " + self.type + " vlucht van " + str(self.cheapest[0]) + " euro naar " + self.to + " \n" \
+                   + self.URL1 + self.cheapest[1][0] + self.middleBit + self.cheapest[1][1] + self.URL2
+
+        msg = f'Subject: {subject}\n\n{body}'
+        server.sendmail(
+            email,
+            email,
+            msg
+        )
+        server.quit()
+
+    def scrape(self):
+        if self.type == 'enkele':
+            for toDate in self.toDates:
+                self.cheapest = self.check_price(toDate, None)
+        elif self.type == 'retour':
+            for toDate in self.toDates:
+                for fromDate in self.fromDates:
+                    self.cheapest = self.check_price(toDate, fromDate)
+        if self.cheapest[0] < self.price:
+            self.send_mail()
+        print(self.to, self.cheapest)
+
+
+driver.quit()
